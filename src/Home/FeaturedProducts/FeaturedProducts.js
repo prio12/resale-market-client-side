efa7b622fcca9f407ce15147d8c6937163d2e52c
MRPrio <maksudurprio.mp@gmail.com>
2022-11-26 19:33:33 +0600

    category added

--- src/Home/FeaturedProducts/FeaturedProducts.js
+++ src/Home/FeaturedProducts/FeaturedProducts.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const FeaturedProducts = () => {
-    return (
-        <div>
-            <h3 className="text-4xl my-6 text-orange-600 text-center font-extrabold">Featured Products</h3>
+const FeaturedProducts = ({ category }) => {
+  const { _id, name, img } = category;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img style={{height:'200px'}} src={img} alt="" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <div className="card-actions">
+          <button className="btn btn-outline btn-error">See More</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedProducts;
--- src/Home/Home/Home.js
+++ src/Home/Home/Home.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import AboutUs from '../AboutUs/AboutUs';
 import FeaturedProducts from '../FeaturedProducts/FeaturedProducts';
 import Slider from '../slider/Slider';
 
 const Home = () => {
+
+  const categoryData = useLoaderData()
     return (
         <div>
+          
           <Slider></Slider>
-          <AboutUs></AboutUs>
-          <FeaturedProducts></FeaturedProducts>  
+          <h3 className="text-4xl my-6 text-orange-600 text-center font-extrabold">Featured Products</h3>
+          {/* <AboutUs></AboutUs> */}
+          <div className='grid my-5  grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+          {
+            categoryData.map(category =><FeaturedProducts
+            key={category._id}
+            category={category}
+            ></FeaturedProducts>)
+          }
+          </div>
+          {/* <FeaturedProducts></FeaturedProducts>   */}
         </div>
     );
 };
