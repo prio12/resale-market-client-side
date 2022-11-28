8cea49767cd7837e475bcb636bec5add2d318d08
MRPrio <maksudurprio.mp@gmail.com>
2022-11-28 20:23:10 +0600

    got booking modal info

--- src/Pages/Collection/BookingModal/BookingModal.js
+++ src/Pages/Collection/BookingModal/BookingModal.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../Contexts/AuthProvider";
 
 const BookingModal = ({bookingItem}) => {
 
+  const {user} = useContext(AuthContext)
+
     const handleBooking = event =>{
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const item = form.bookingItemName.value;
+        const price = form.price.value;
+        const number = form.number.value;
+        const location = form.location.value;
+
+        const booking = {
+
+          userName:name,
+          userEmail:email,
+          bookingItem:item,
+          productPrice:price,
+          userNumber:number,
+          userLocation:location
+        }
+        console.log(booking)
     }
   return (
     <>
@@ -20,10 +41,12 @@ const BookingModal = ({bookingItem}) => {
             You selected {bookingItem.name}!
           </h3>
          <form onSubmit={handleBooking} className="grid grid-cols-1 gap-4 mt-10">
-         <input type="text" placeholder="Type here" className="input input-bordered input-info w-full" />
-         <input type="text" placeholder="Type here" className="input input-bordered input-info w-full" />
-         <input type="text" placeholder="Type here" className="input input-bordered input-info w-full" />
-         <input type="text" placeholder="Type here" className="input input-bordered input-info w-full" />
+         <input type="text" name="name" defaultValue={user?.displayName} disabled className="input input-bordered input-info w-full" />
+         <input type="email" name="email" defaultValue={user?.email} disabled className="input input-bordered input-info w-full" />
+         <input type="text" name="bookingItemName" defaultValue={bookingItem.name} disabled className="input input-bordered input-info w-full" />
+         <input type="text" name="price" defaultValue={bookingItem.resalePrice} disabled className="input input-bordered input-info w-full" />
+         <input type="text" name="number" placeholder="Enter your phone number"  className="input input-bordered input-info w-full" />
+         <input type="text" name="location" placeholder="meeting location"  className="input input-bordered input-info w-full" />
          <input type="submit" value="Submit" className="btn btn-accent w-full" />
          </form>
         </div>
