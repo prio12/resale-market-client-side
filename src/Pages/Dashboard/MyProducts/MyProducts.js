ecf3dafcbc213606f4313156140aa4935cf20d6a
MRPrio <maksudurprio.mp@gmail.com>
2022-11-30 07:05:11 +0600

    delete btn added

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../../Contexts/AuthProvider';
 
 const MyProducts = () => {
     const {user} = useContext(AuthContext)
     const url = `http://localhost:5000/collection?email=${user?.email}`
 
-    const {data: products = []} = useQuery({
+    const {data: products = [],refetch} = useQuery({
         queryKey:['bookings', user?.email],
         queryFn:async () =>{
             const res = await fetch(url);
@@ -15,9 +16,43 @@ const MyProducts = () => {
             return data;
         }
     })
+
+    const handleDelete = id =>{
+        const agree = window.confirm('Are u sure u wanna delete this product?')
+        if(agree){
+            fetch(`http://localhost:5000/collection/delete/${id}`,{
+                method:"DELETE"
+            })
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data)
+                if(data.deletedCount > 0){
+                    toast.success('deleted successfully')
+                    refetch()
+                }
+            })
+        }
+    }
     return (
         <div>
             <h3 className='text-2xl font-bold text-center my-10'>Your Products {products.length}</h3>
+            <div>
+                {
+                    products.map(product =><div key={product._id} className="card shadow-xl">
+                    <figure><img src={product.image} alt="Shoes" /></figure>
+                    <div className="card-body text-center">
+                      <h2 className="text-2xl font-bold text-center">
+                       {product.name}
+                      </h2>
+                      <p>Sales Status :</p>
+                      <div className="card-actions justify-end">
+                        <button onClick={() => handleDelete(product._id)} className='btn btn-outline'>Delete</button>
+                        <button className='btn btn-outline'>Advertise</button>
+                      </div>
+                    </div>
+                  </div>)
+                }
+            </div>
         </div>
     );
 };
